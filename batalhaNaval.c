cca4441a50ddc59265e7b13f1756ab6ca3349844
Welington Aguiar <85399106+WelingtonAguiar@users.noreply.github.com>
2025-04-25 02:43:23 +0000

    nive_basico

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,93 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+// Definindo constantes para facilitar mudanças futuras e legebilidade
+#define tamanho_tabuleiro 10
+#define tamanho_navio 3
+#define navio 3
+#define agua 0
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+int main() 
+{
+    int tabuleiro[tamanho_tabuleiro][tamanho_tabuleiro];
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    for (int i = 0 ; i < tamanho_tabuleiro ; i++)
+    {
+        for (int j = 0 ; j < tamanho_tabuleiro ; j++)
+        {
+            tabuleiro[i][j] = agua;
+        }
+    }
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    int linha_navio_horizontal = 2,
+        coluna_navio_horizontal = 4;
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    int linha_navio_vertical = 5,
+        coluna_navio_vertical = 1;    
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+        if (coluna_navio_horizontal + tamanho_navio <= tamanho_tabuleiro)
+        {
+            int colisao = 0;
+            for (int i = 0 ; i < tamanho_navio ; i++)
+            {
+                if (tabuleiro[linha_navio_horizontal][coluna_navio_horizontal + i] == navio)
+                {
+                    colisao = 1;
+                    break;
+                }
+            }
+
+            if (!colisao)
+            {
+                for (int i = 0 ; i < tamanho_navio ; i++)
+                {
+                    tabuleiro[linha_navio_horizontal][coluna_navio_horizontal + i] = navio; 
+                }
+            }
+        }
+
+        if (linha_navio_vertical + tamanho_navio <= tamanho_tabuleiro)
+        {
+            int colisao = 0;
+            for (int i = 0 ; i < tamanho_navio ; i++)
+            {
+                if (tabuleiro[linha_navio_vertical][coluna_navio_vertical + i] == navio)
+                {
+                    colisao = 1;
+                    break;
+                }
+            }
+
+            if (!colisao)
+            {
+                for (int i = 0 ; i < tamanho_navio ; i++)
+                {
+                    tabuleiro[linha_navio_vertical + i][coluna_navio_vertical] = navio; 
+                }
+            }
+        }
+
+        printf("\n     ");
+        printf("\n      ***** BATALHA NAVAL *****\n\n");
+        printf("    ");
+
+        for (char letra = 'A' ; letra < 'A' + tamanho_tabuleiro ; letra++)
+        {
+            printf(" %c ", letra);
+        }
+
+        printf("\n");
+
+        for (int i = 0 ; i < tamanho_tabuleiro ; i++)
+        {
+            printf("%2d  ", i + 1);
+
+            for(int j = 0 ; j < tamanho_tabuleiro ; j++)
+            {
+                printf(" %d ", tabuleiro[i][j]);
+            }
+
+            printf("\n");
+        }
 
     return 0;
 }
