67b1075e645aa79fd5b18db8a9477e9cd2a77248
Welington Aguiar <85399106+WelingtonAguiar@users.noreply.github.com>
2025-04-27 02:04:58 +0000

    Desafio atualizado: Nível Intermediário

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,93 +1,115 @@
 #include <stdio.h>
 
-// Definindo constantes para facilitar mudanças futuras e legebilidade
-#define tamanho_tabuleiro 10
-#define tamanho_navio 3
-#define navio 3
-#define agua 0
+// Definindo constantes para o tamanho do tabuleiro e valores de identificação
+#define TAMANHO_TABULEIRO 10
+#define TAMANHO_NAVIO 3
+#define NAVIO 3
+#define AGUA 0
 
 int main() 
 {
-    int tabuleiro[tamanho_tabuleiro][tamanho_tabuleiro];
+    // Matriz representando o tabuleiro
+    int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO];
 
-    for (int i = 0 ; i < tamanho_tabuleiro ; i++)
-    {
-        for (int j = 0 ; j < tamanho_tabuleiro ; j++)
-        {
-            tabuleiro[i][j] = agua;
+    // Inicializando o tabuleiro com água
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
+            tabuleiro[i][j] = AGUA;
         }
     }
 
-    int linha_navio_horizontal = 2,
-        coluna_navio_horizontal = 4;
+    // Coordenadas para posicionar navios
+    int linha_navio_hor = 2, coluna_navio_hor = 4; // Horizontal
+    int linha_navio_ver = 5, coluna_navio_ver = 1; // Vertical
+    int linha_navio_diag1 = 0, coluna_navio_diag1 = 0; // Diagonal principal (↘)
+    int linha_navio_diag2 = 0, coluna_navio_diag2 = 9; // Diagonal secundária (↙)
 
-    int linha_navio_vertical = 5,
-        coluna_navio_vertical = 1;    
-
-        if (coluna_navio_horizontal + tamanho_navio <= tamanho_tabuleiro)
-        {
-            int colisao = 0;
-            for (int i = 0 ; i < tamanho_navio ; i++)
-            {
-                if (tabuleiro[linha_navio_horizontal][coluna_navio_horizontal + i] == navio)
-                {
-                    colisao = 1;
-                    break;
-                }
-            }
-
-            if (!colisao)
-            {
-                for (int i = 0 ; i < tamanho_navio ; i++)
-                {
-                    tabuleiro[linha_navio_horizontal][coluna_navio_horizontal + i] = navio; 
-                }
+    // Posicionando navio horizontal
+    if (coluna_navio_hor + TAMANHO_NAVIO <= TAMANHO_TABULEIRO) {
+        int colisao = 0;
+        for (int i = 0; i < TAMANHO_NAVIO; i++) {
+            if (tabuleiro[linha_navio_hor][coluna_navio_hor + i] == NAVIO) {
+                colisao = 1;
+                break;
             }
         }
-
-        if (linha_navio_vertical + tamanho_navio <= tamanho_tabuleiro)
-        {
-            int colisao = 0;
-            for (int i = 0 ; i < tamanho_navio ; i++)
-            {
-                if (tabuleiro[linha_navio_vertical][coluna_navio_vertical + i] == navio)
-                {
-                    colisao = 1;
-                    break;
-                }
-            }
-
-            if (!colisao)
-            {
-                for (int i = 0 ; i < tamanho_navio ; i++)
-                {
-                    tabuleiro[linha_navio_vertical + i][coluna_navio_vertical] = navio; 
-                }
+        if (!colisao) {
+            for (int i = 0; i < TAMANHO_NAVIO; i++) {
+                tabuleiro[linha_navio_hor][coluna_navio_hor + i] = NAVIO;
             }
         }
+    }
 
-        printf("\n     ");
-        printf("\n      ***** BATALHA NAVAL *****\n\n");
-        printf("    ");
-
-        for (char letra = 'A' ; letra < 'A' + tamanho_tabuleiro ; letra++)
-        {
-            printf(" %c ", letra);
+    // Posicionando navio vertical
+    if (linha_navio_ver + TAMANHO_NAVIO <= TAMANHO_TABULEIRO) {
+        int colisao = 0;
+        for (int i = 0; i < TAMANHO_NAVIO; i++) {
+            if (tabuleiro[linha_navio_ver + i][coluna_navio_ver] == NAVIO) {
+                colisao = 1;
+                break;
+            }
         }
+        if (!colisao) {
+            for (int i = 0; i < TAMANHO_NAVIO; i++) {
+                tabuleiro[linha_navio_ver + i][coluna_navio_ver] = NAVIO;
+            }
+        }
+    }
 
+    // Posicionando navio diagonal principal (↘)
+    if (linha_navio_diag1 + TAMANHO_NAVIO <= TAMANHO_TABULEIRO &&
+        coluna_navio_diag1 + TAMANHO_NAVIO <= TAMANHO_TABULEIRO) {
+        
+        int colisao = 0;
+        for (int i = 0; i < TAMANHO_NAVIO; i++) {
+            if (tabuleiro[linha_navio_diag1 + i][coluna_navio_diag1 + i] == NAVIO) {
+                colisao = 1;
+                break;
+            }
+        }
+        if (!colisao) {
+            for (int i = 0; i < TAMANHO_NAVIO; i++) {
+                tabuleiro[linha_navio_diag1 + i][coluna_navio_diag1 + i] = NAVIO;
+            }
+        }
+    }
+
+    // Posicionando navio diagonal secundária (↙)
+    if (linha_navio_diag2 + TAMANHO_NAVIO <= TAMANHO_TABULEIRO &&
+        coluna_navio_diag2 - (TAMANHO_NAVIO - 1) >= 0) {
+
+        int colisao = 0;
+        for (int i = 0; i < TAMANHO_NAVIO; i++) {
+            if (tabuleiro[linha_navio_diag2 + i][coluna_navio_diag2 - i] == NAVIO) {
+                colisao = 1;
+                break;
+            }
+        }
+        if (!colisao) {
+            for (int i = 0; i < TAMANHO_NAVIO; i++) {
+                tabuleiro[linha_navio_diag2 + i][coluna_navio_diag2 - i] = NAVIO;
+            }
+        }
+    }
+
+    // Exibindo o tabuleiro
+    printf("\n      ***** BATALHA NAVAL - NIVEL AVENTUREIRO *****\n\n");
+
+    // Cabeçalho de letras
+    printf("    ");
+    for (char letra = 'A'; letra < 'A' + TAMANHO_TABULEIRO; letra++) {
+        printf(" %c ", letra);
+    }
+    printf("\n");
+
+    // Corpo do tabuleiro
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+        printf("%2d  ", i + 1); // Número da linha
+        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
+            printf(" %d ", tabuleiro[i][j]);
+        }
         printf("\n");
-
-        for (int i = 0 ; i < tamanho_tabuleiro ; i++)
-        {
-            printf("%2d  ", i + 1);
-
-            for(int j = 0 ; j < tamanho_tabuleiro ; j++)
-            {
-                printf(" %d ", tabuleiro[i][j]);
-            }
-
-            printf("\n");
-        }
+    }
 
     return 0;
 }
